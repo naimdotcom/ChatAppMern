c5a6914cfaf582d7d36f2e561c0ff78f561335ca
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-01-03 03:07:26 +0600

    fix: update Navbar import path in NavFooter component

--- frontend/src/pages/NavFooter.jsx
+++ frontend/src/pages/NavFooter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/Index";
 
 function NavFooter() {
   return (
