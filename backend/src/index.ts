633168245483993969f975be81703f6a2cf7bb26
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-01-04 00:13:41 +0600

    fix: remove commented-out CORS origin definition in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const origin = process.env.FRONTEND_URL || "http://localhost:5173";
+// const origin = process.env.FRONTEND_URL || "http://localhost:5173";
 const _dirname = path.dirname(__filename);
 
 app.use(express.json());
